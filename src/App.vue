d680e182ded5ad3252abd8f398be439889c17c7a
welves de paula <welvesdepaula@gmail.com>
2022-07-04 17:33:19 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,28 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <!-- npm install @material/top-app-bar &&
+  npm install @material/banner &&
+  npm install @material/button &&
+  npm install @material/fab &&
+  npm install @material/card &&
+  npm install @material/checkbox &&
+  npm install @material/chips &&
+  npm install @material/data-table &&
+  npm install @material/dialog &&
+  npm install @material/image-list &&
+  npm install @material/list &&
+  npm install @material/menu &&
+  npm install @material/drawer &&
+  npm install @material/drawer &&
+  npm install @material/radio &&
+  npm install @material/snackbar &&
+  npm install @material/slider &&
+  npm install @material/switch &&
+  npm install @material/tab-bar &&
+  npm install @material/textfield &&
+  npm install @material/tooltip -->
+  <router-view />
 </template>
 
 <style>
